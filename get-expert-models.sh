8523c01516e5407a1949c804fcf05ebd0f4968d9
Nancy Day <nday@uwaterloo.ca>
2023-09-13 13:37:19 -0400

    added script for getting expert models

---
+++ get-expert-models.sh
@@ -0,0 +1,20 @@
+# These commands were created from running catalyst to find all Alloy models on the list
+# and then going through the log.txt file to extract the git clone commands for the ones 
+# that Elias used as his expert models, which are listed in the README at:
+# https://github.com/WatForm/static-profiling-of-alloy-models
+
+git clone https://github.com/ogiroux/talks.git 2scxlb3tbo5bmvmwplglqils7a5uarmx-talks
+git clone https://github.com/atdyer/alloy.git 3zltn65gds66b6f4q3lvbtgdkb6snmuu-alloy
+git clone https://github.com/pron/amazon-snapshot-spec.git 7d25ioxqmue65lp6ntzz735gpbg4fmgq-amazon-snapshot-spec
+git clone https://github.com/nmacedo/MSV.git 7z32luflamhdcixvt6nwznnud4oi6dbr-MSV
+git clone https://github.com/BGCX261/zigbee-alloy-svn-to-git.git lkicptlz3eklrbu7ppmltlkebwrvzhdq-zigbee-alloy-svn-to-git
+git clone https://github.com/hkhojasteh/CANBus.git oujlbmnutprdhddstyudppn7t35n43os-CANBus
+git clone https://github.com/atdyer/alloy.git 3zltn65gds66b6f4q3lvbtgdkb6snmuu-alloy
+git clone https://github.com/NVlabs/litmustestgen.git 5x4l2fj5nfbq3cz2dumwdt57g3kig3rd-litmustestgen
+git clone https://github.com/AlloyTools/models.git gumxtrzzbkrtwi7jtwyu7eibi3fwhgmf-models
+git clone https://github.com/nadeshr/weak_atomics.git x7tjf3r7wnejcplj75s2o6im45kjodhs-weak_atomics
+git clone https://github.com/naorinh/TransForm.git x7t75qqe5fr6uzitot5sdu63o7drnur5-TransForm
+
+mkdir chord-pamela-zave
+cd chord-pamela-zave
+wget https://www.pamelazave.com/chordfull.als
