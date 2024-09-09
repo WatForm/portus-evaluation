d8bea8f5065117b1350acb3e11d452f00ff1c9f3
Ryan Dancy <ryan@keal.ca>
2024-09-09 22:45:08 +0200

    Add script to select worst signature and allow scaling it

--- scaling_eval/__main__.py
+++ scaling_eval/__main__.py
@@ -4,24 +4,34 @@ import logging
 import sys
 
 from scaling_eval import run
-from scaling_eval.scale import scale_all_sigs
+from scaling_eval.scale import scale_all_sigs, scale_sig
 from setup_scripts.config import ALLOY_JAR, PORTUS_JAR
 
 from eval_portus import PORTUS_METHODS
 
 
-def read_models(models_filename):
+def read_models(models_filename, specify_sigs=False):
     with open(models_filename, "r") as models_file:
         reader = csv.reader(models_file)
         next(reader) # skip header
-        for model_filename, command_num in reader:
-            yield model_filename, int(command_num)
+        if specify_sigs:
+            for model_filename, command_num, sig_num in reader:
+                yield model_filename, int(command_num), int(sig_num)
+        else:
+            for model_filename, command_num in reader:
+                yield model_filename, int(command_num)
 
 
-def scale_all_models(runner: run.Runner, methods, models_filename, scopes=range(1, 21), timeout_s=60, cpu_time=True):
-    for filename, command_num in read_models(models_filename):
-        yield from scale_all_sigs(
-            runner, methods, filename, command_num, scopes=scopes, timeout_s=timeout_s, cpu_time=cpu_time)
+def scale_all_models(
+        runner: run.Runner, methods, models_filename, scopes=range(1, 21), timeout_s=60, cpu_time=True, specify_sigs=False):
+    if specify_sigs:
+        for filename, command_num, sig_num in read_models(models_filename, specify_sigs=True):
+            yield from scale_sig(
+                runner, methods, filename, command_num, sig_num, scopes=scopes, timeout_s=timeout_s, cpu_time=cpu_time)
+    else:
+        for filename, command_num in read_models(models_filename, specify_sigs=False):
+            yield from scale_all_sigs(
+                runner, methods, filename, command_num, scopes=scopes, timeout_s=timeout_s, cpu_time=cpu_time)
 
 
 def write_results_to_csv(csv_filename, scale_iter):
@@ -41,6 +51,8 @@ def main():
     parser.add_argument("--java", default="java", help="The Java command to run Alloy with.")
     parser.add_argument("--jar", default=str(ALLOY_JAR.absolute()), help="Path to the Portus jar.")
     parser.add_argument("--models", default="models-supported-command.txt", help="Path to models-supported-command.txt.")
+    parser.add_argument("--sigs", default="all", choices=["all", "specified"],
+        help="Run all sigs (all) or specified in the models csv (specified).")
     parser.add_argument("--out", default="scale.csv", help="Output CSV filename.")
     parser.add_argument("--start", type=int, default=1, help="Starting scope of a model to try.")
     parser.add_argument("--end", type=int, default=30, help="Maximum scope of a model to try.")
@@ -61,7 +73,9 @@ def main():
 
     scopes = range(args.start, args.end+1, args.step)
     cpu_time = args.cpu_time == "true"
-    scale_iter = scale_all_models(runner, args.methods, args.models, scopes, timeout_s=args.timeout, cpu_time=cpu_time)
+    specify_sigs = args.sigs == "specified"
+    scale_iter = scale_all_models(
+        runner, args.methods, args.models, scopes, timeout_s=args.timeout, cpu_time=cpu_time, specify_sigs=specify_sigs)
     write_results_to_csv(args.out, scale_iter)
 
 
---
+++ scaling_eval/select_worst.py
@@ -0,0 +1,47 @@
+"""
+Given a CSV file produced by scaling_eval, for each (model, command) pair, find the
+worst-performing sig as measured by highest scope achieved before timeout (among any method)
+and output to another CSV file with columns (model, command, sig).
+"""
+
+from collections import defaultdict
+import csv
+import random
+import sys
+
+# specific seed for reproducibility
+random.seed(1)
+
+if len(sys.argv) <= 2:
+    print("Usage: select_worst.py <scale_csv_filename> <output_csv_filename>")
+    sys.exit(-1)
+
+scale_csv_filename = sys.argv[1]
+output_csv_filename = sys.argv[2]
+
+# For each (model, command), map sig -> list of scopes achieved
+command_to_sig_to_scopes = defaultdict(lambda: defaultdict(list))
+with open(scale_csv_filename, "r") as scale_csv:
+    reader = csv.reader(scale_csv)
+    next(reader) # skip header
+    for method, model, command, sig, scope, time in reader:
+        if time != "timeout":
+            command_to_sig_to_scopes[model, command][sig].append(scope)
+
+command_to_worst_sig = {}
+for (model, command), sig_to_scopes in command_to_sig_to_scopes.items():
+    sig_to_max_scope = {
+        sig: max(scopes, default=-1)
+        for sig, scopes in sig_to_scopes.items()
+    }
+    worst_max_scope = min(sig_to_max_scope.values())
+    worst_sigs = [sig for sig, max_scope in sig_to_max_scope.items() if max_scope == worst_max_scope]
+    # randomly break any ties
+    worst_sig = random.choice(worst_sigs)
+    command_to_worst_sig[model, command] = worst_sig
+
+with open(output_csv_filename, "w") as output_csv:
+    writer = csv.writer(output_csv)
+    writer.writerow(["model", "command_number", "sig_number"])
+    for (model, command), sig in sorted(command_to_worst_sig.items()):
+        writer.writerow([model, command, sig])
