85c1af6ec65e846556fd43173d16d65ed56fdf1c
nancyday <nday@uwaterloo.ca>
2025-02-19 15:22:47 -0500

    adding scripts with instructions for testing alloy2smt

---
+++ test-alloy2smt-results.md
@@ -0,0 +1,15 @@
+# Results of test_alloy2smt.py
+
+## Instructions for generating these results are in test_alloy2smt.py
+
+## File not found
+
+Ran alloy2smt on all these models in their own directories and still can't find subfiles in the same directory.
+
+expert-models/2scxlb3tbo5bmvmwplglqils7a5uarmx-talks/scc_mp.als
+expert-models/3zltn65gds66b6f4q3lvbtgdkb6snmuu-alloy/fullsub2.als
+expert-models/3zltn65gds66b6f4q3lvbtgdkb6snmuu-alloy/question.als
+expert-models/7z32luflamhdcixvt6nwznnud4oi6dbr-MSV/Systems/CD2DBS_keys/dbs_inst.als
+expert-models/7z32luflamhdcixvt6nwznnud4oi6dbr-MSV/Systems/CD2DBS_simple/dbs_inst.als
+expert-models/7z32luflamhdcixvt6nwznnud4oi6dbr-MSV/Systems/ElevatorSPL/elevator_spl_events.als
+expert-models/lkicptlz3eklrbu7ppmltlkebwrvzhdq-zigbee-alloy-svn-to-git/trunk/base/random_event.als
---
+++ test-alloy2smt.py
@@ -0,0 +1,134 @@
+#!/usr/local/bin/python3
+
+"""
+    This script is our process to test the alloy2smt tool
+    documented and repo linked in:
+
+    A new relational solver for the Alloy Analyzer
+    Mudathir Mohamed, Baoluo Meng, Andrew Reynolds and Cesare Tinelli
+    https://homepage.divms.uiowa.edu/~mahgoubyahia/pdf/crs.pdf
+    repo: https://github.com/CVC4/org.Alloytools.Alloy
+
+    Set up before running this script:
+    
+    1) install cvc4 (code does not work with CVC5, which uses different names for CVC4 native input language (https://cvc4.github.io/cvc4-native-input-language.html)
+        set cvc4 variable to its executable
+
+    2) build tool:
+        wget https://github.com/CVC4/org.alloytools.alloy/archive/refs/tags/v5.0.0.5.tar.gz
+        (this release is from Sep 23, 2019)
+        gunzip v5.0.0.5.tar.gz
+        tar -xf v5.0.0.5.tar
+        cd org.alloytools.alloy-5.0.0.5/alloy2smt/
+        jenv local 11.0
+        jenv enable-plugin export (to make sure JAVA_HOME set correctly)
+        chmod +x ./gradlew
+        ./gradlew build
+
+    3) set up portus-evaluation Benchmark set 
+        git clone https://github.com/WatForm/portus-evaluation.git (which this python script is within)
+        edit Makefile to take out 'remove-unsupported.py' and 'compile-top-level-file-list.py' (which requires portus)
+        make (converts files to Alloy 6, etc.)
+        wc -l models-supported.txt (to make sure 74 models)
+
+    4) set 'alloy2smt' variable to 'java -jar ../org.alloytools.alloy-5.0.0.5/alloy2smt/build/libs/alloy2smt_with_dependencies.jar -i '  (".." assumes this script is in a sister directory)
+
+    5) run this script (python3 test-alloy2smt.py)
+
+    6) results can be found in test-alloy2smt-results.md
+"""
+
+
+
+# starting with 74 models -- removed remove-unsupported in Makefile
+import subprocess
+
+models = open("models-supported.txt","r")
+
+alloy2smt = "java -jar ../org.alloytools.alloy-5.0.0.5/alloy2smt/build/libs/alloy2smt_with_dependencies.jar -o tmp.smt2 -i "
+cvc4 = "cvc4 "
+
+file_cannot_be_found = 0
+not_found = []
+unsupported = 0 
+supported = 0
+total = 0
+java_runtime_exception = 0
+java_indexoutofbounds_exception = 0
+other_err = 0 
+cvc4_unsupported = 0
+cvc4_error = 0
+cvc4_unknown = 0
+cvc4_sat = 0
+cvc4_unsat = 0
+
+
+for line in models:
+    total += 1
+    with subprocess.Popen(alloy2smt+line.strip(), stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True,shell=True) as p:
+        # output is the output stream from calling george on the file
+        (output, err) = p.communicate()
+        if 'File cannot be found.' in err:
+            file_cannot_be_found += 1
+            not_found.append(line)
+            #print('File cannot be found.')
+        elif 'java.lang.UnsupportedOperationException' in err:
+            unsupported += 1
+            #print('java.lang.UnsupportedOperationException')
+        elif 'java.lang.RuntimeException' in err:
+            java_runtime_exception += 1
+            #print('java.lang.RuntimeException')
+        elif 'java.lang.IndexOutOfBoundsException' in err:
+            java_indexoutofbounds_exception += 1
+            #print('java.lang.IndexOutOfBoundsException')
+        elif err:
+            other_err += 1
+            #print(err)
+        else:
+            supported += 1
+
+            # write to a temp file and provide as command-line input
+
+            # cvc4 installed on brew won't work on x86_64
+            with subprocess.Popen(cvc4 +' tmp.smt2', stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True,shell=True) as q:
+                (output, err) = q.communicate()
+                if 'unsupported' in output:
+                    cvc4_unsupported += 1
+                elif 'error' in err:
+                    cvc4_error += 1
+                elif 'unknown' in output:
+                    cvc4_unknown += 1
+                elif 'sat' in output:
+                    cvc4_sat += 1
+                elif 'unsat' in output:
+                    cvc4_unsat += 1
+                else:
+                    print('-----')
+                    print(line.strip())
+                    print(output)
+                    print('-----')
+                    print(err)
+                    print('-----')
+
+
+print("file cannot be found: "+str(file_cannot_be_found))
+for i in not_found:
+    print(i)
+
+    
+print("java.lang.UnsupportedOperationException: "+str(unsupported))
+print("java.lang.RuntimeException: "+str(java_runtime_exception))
+print("java.lang.IndexOutOfBoundsException: "+ str(java_indexoutofbounds_exception))
+print("other error: "+str(other_err))
+print("supported to translate to cvc: "+str(supported))
+print('cvc4 unsupported: '+str(cvc4_unsupported))
+print('cvc4 error: '+str(cvc4_error))
+print('cvc4 unknown: '+str(cvc4_unknown))
+print('cvc4 sat: '+str(cvc4_sat))
+print('cvc4 unsat: '+str(cvc4_unsat))
+
+print("total: "+str(total))          
+
+models.close()
+
+
