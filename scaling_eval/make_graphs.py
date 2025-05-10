0ab8e607ba7042f36133dd3832b8cc18cf99ac28
Ryan Dancy <ryan@keal.ca>
2025-05-10 19:51:15 +0100

    Configure make_graphs.py to generate the graphs we need

--- scaling_eval/make_graphs.py
+++ scaling_eval/make_graphs.py
@@ -25,30 +25,57 @@ def parse_results(results_paths):
 
 
 def plot(method_to_results, title, path, timeout=60):
+    SMALL_SIZE = 12
+    MEDIUM_SIZE = 16
+
+    plt.figure(dpi=600)
+    plt.rc('font', size=MEDIUM_SIZE)
+    plt.rc('axes', titlesize=MEDIUM_SIZE)
+    plt.rc('axes', labelsize=MEDIUM_SIZE)
+    plt.rc('xtick', labelsize=SMALL_SIZE)
+    plt.rc('ytick', labelsize=SMALL_SIZE)
+    plt.rc('legend', fontsize=SMALL_SIZE)
+
     portus_scopes = [res["scope"] for res in method_to_results["portus-full"]]
     portus_times = [res["time"] if res["time"] != "timeout" else timeout for res in method_to_results["portus-full"]]
     kodkod_scopes = [res["scope"] for res in method_to_results["kodkod"]]
     kodkod_times = [res["time"] if res["time"] != "timeout" else timeout for res in method_to_results["kodkod"]]
-    plt.plot(portus_scopes, portus_times, label="portus")
-    plt.plot(kodkod_scopes, kodkod_times, label="kodkod")
-    if portus_times[-1] == "timeout":
-        plt.plot(portus_scopes[-1], timeout, "rx", label="timeout")
-    if kodkod_times[-1] == "timeout":
-        plt.plot(kodkod_scopes[-1], timeout, "rx", label="timeout")
+    plt.plot(portus_scopes, portus_times, label="Portus", linestyle="-")
+    plt.plot(kodkod_scopes, kodkod_times, label="Kodkod", linestyle="--")
+    if portus_times[-1] == timeout:
+        plt.plot(portus_scopes[-1], timeout, "rx")
+    if kodkod_times[-1] == timeout:
+        plt.plot(kodkod_scopes[-1], timeout, "rx")
     plt.title(title)
-    plt.legend(loc="upper left")
+    plt.grid()
+    plt.legend()
     plt.xlabel("Scope")
     plt.ylabel("Solving time (s)")
     plt.yscale("log")
-    # plt.hlines(timeout, *plt.xlim(), linestyle="--", linewidth=1.0, color="black")
-    # plt.ylim(top=timeout * 1.1)
-    plt.savefig(path)
+    plt.savefig(path, bbox_inches="tight")
     plt.clf()
+
+
+TITLE_OVERRIDE = {
+    ("function.als", 3, 1): "Scalability of functions, scaling A, keeping B at 16",
+    ("function.als", 3, 2): "Scalability of functions, scaling B, keeping A at 16",
+    ("function.als", 4, 1): "Scalability of functions, scaling A, keeping B at 32",
+    ("function.als", 4, 2): "Scalability of functions, scaling B, keeping A at 32",
+    ("relation.als", 3, 1): "Scalability of relations, scaling A",
+    ("relation.als", 3, 2): "Scalability of relations, scaling B",
+    ("function-composition.als", 1, 1): "Scalability of function composition, scaling A",
+    ("order.als", 2, 1): "Scalability of the ordering module, scaling A",
+    ("card.als", 1, 1): "Scalability of cardinality, scaling A",
+    ("tc-function.als", 1, 1): "Scalability of transitive closure over functions, scaling A",
+    ("tc-relation.als", 1, 1): "Scalability of transitive closure over relations, scaling A",
+}
 
 
 def make_title_and_path(model_path, command, sig):
     model = Path(model_path).name
     title = f"{model}, cmd {command}, sig {sig}"
+    if (model, command, sig) in TITLE_OVERRIDE:
+        title = TITLE_OVERRIDE[model, command, sig]
     path = Path(FIGS_PATH) / f"{model}-cmd{command}-sig{sig}.png"
     return title, str(path)
 
